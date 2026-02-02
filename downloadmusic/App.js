5736111988dab972ca41390453f77b62c9a4e534
ShionGT <shion.toda@gmail.com>
2026-02-02 11:22:19 +0900

    Update App.js

--- downloadmusic/App.js
+++ downloadmusic/App.js
@@ -2,14 +2,6 @@ import { Button, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    // <View style={styles.header}>
-    //   <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Music Downloader</Text>
-    // </View>,
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working your app!</Text>
-    //   <Button title="Download Music" onPress={() => alert('Downloading...')} />
-    //   <StatusBar style="auto" />
-    // </View>
     <View style={styles.container}>
       <Text>Hello</Text>
       <Pressable>Button</Pressable>
